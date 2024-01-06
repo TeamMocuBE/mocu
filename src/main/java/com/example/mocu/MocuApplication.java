2c9830446d4d63bb411984b22fc7c8d96041fa21
seongjunnoh <seongjunnoh>
2024-01-06 15:14:09 +0900

    Initial commit

---
+++ src/main/java/com/example/mocu/MocuApplication.java
@@ -0,0 +1,13 @@
+package com.example.mocu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MocuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MocuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mocu/MocuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mocu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MocuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
