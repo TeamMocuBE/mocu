c045dcc4642f47e3dc5b74ba886331871ea7d565
twocastlejean-20230606-3 <sjbin0301@gmail.com>
2024-01-23 17:55:53 +0900

    mypage.registerAddress

---
+++ src/main/java/com/example/mocu/Dto/address/PostAddressRequest.java
@@ -0,0 +1,15 @@
+package com.example.mocu.Dto.address;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostAddressRequest {
+    private String name;
+    private String address;
+}
