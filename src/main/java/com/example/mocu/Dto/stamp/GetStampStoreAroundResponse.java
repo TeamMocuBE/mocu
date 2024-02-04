8250656d466ec71b1486ab6703f3520452e52311
seongjunnoh <seongjunnoh>
2024-02-05 04:07:39 +0900

    feat 적립 페이지 구현 중

---
+++ src/main/java/com/example/mocu/Dto/stamp/GetStampStoreAroundResponse.java
@@ -0,0 +1,14 @@
+package com.example.mocu.Dto.stamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetStampStoreAroundResponse {
+    // 가게 대표 사진, 가게 이름, numOfStamp, maxStamp, rating, reward, numOfCouponAvailable
+}
